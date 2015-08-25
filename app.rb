3feaece1f7cdc4153100baf70a97dbec5ce25cbc
jessicashannon <jessdahling@gmail.com>
2015-08-25 17:25:15 -0400

    done

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
 require_relative 'config/environment'
 
 class GameApp < Sinatra::Base
+
   get '/rps/:play' do
-    # Your app code goes here
+    @game = RPSGame.new(params[:play])
     erb :rps_game
   end
 end
+
+# Integrate your RPSGame class with the Sinatra application in app.rb.
+#
+# The application will serve 1 route: GET requests to  the path /rps/:play.
+# The value of :play will be used as the player's throw (rock, paper, or
+# scissors).
+#
+# The Sinatra application should serve an HTML page that displays the move that
+# the player made, the move that the computer made, and whether the player won,
+# lost, or tied.
--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,2 +1,47 @@
+require 'pry'
+
 class RPSGame
+
+attr_accessor :won, :lost, :tied, :computer_play, :play, :computer_play
+@@choices = [:rock, :paper, :scissors]
+
+  def initialize(play)
+    @computer_play = computer_play
+    @play = play
+    unless @@choices.include?(@play.to_sym)
+      raise PlayTypeError
+    end
+  end
+
+  def self.valid_play?(play)
+    @@choices.include?(play)
+  end
+
+  def computer_play
+    @computer_play = @@choices.sample
+  end
+
+  def won?
+    if computer_play == :scissors && @play == :rock
+      @won = true
+    elsif computer_play == :rock && @play == :paper
+      @won = true
+    elsif computer_play == :paper && @play == :scissors
+      @won = true
+    else
+      @won = false
+    end
+  end
+
+  def tied?
+    @tied = (computer_play == @play)
+  end
+
+  def lost?
+    @lost = !(won? || tied?)
+  end
+
+  class PlayTypeError < StandardError
+  end
+
 end
